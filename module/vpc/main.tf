9efb7a7bb584c76dbcff19055f0dc03a1b137df4
hashmisa <mujeer472@gmail.com>
2024-09-11 19:47:33 +0530

    updated

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 1.9.0"
   required_providers {
     aws = {
-      version = ">= 5.99.0"
+      version = ">= 5.66.0"
       source = "hashicorp/aws"
     }
   }
