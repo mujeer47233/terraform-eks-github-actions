814a38a6a392be0fb67a32f304314f6668e31fee
hashmisa <mujeer472@gmail.com>
2024-09-12 13:25:10 +0530

    updated

--- module/eks/main.tf
+++ module/eks/main.tf
@@ -110,7 +110,7 @@ resource "aws_iam_role_policy_attachment" "Amazon_EKS_ServicePolicy" {
 # EKS Node group
 
 resource "aws_eks_node_group" "private-nodes" {
-  cluster_name = var.cluster_name
+  cluster_name = aws_eks_cluster.dxlab.name
   node_group_name = var.cluster_node_group_name
   node_role_arn = aws_iam_role.nodes.arn
   remote_access {
